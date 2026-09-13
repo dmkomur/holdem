e85d002e12d367bcae0d962580755e8b80a60240
Dmytro Komurko <komur.d@gmail.com>
2026-09-13 15:06:36 +0200

    fix: switch cssMinify to esbuild

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header: React.FC = () => {
     return (
         <header className="app-header">
             <a href="/" className="brand-logo" aria-label="Go to lobby">
-                <h1 className="brand-title">SPACE TEXAS</h1>
+                <h1 className="brand-title">♦ SPACE TEXAS ♦</h1>
             </a>
         </header>
     );
